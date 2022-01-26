4b5713871d310346ac4eaa3e67ccec9b3d304151
rohanseth497 <rohan.seth497@gmail.com>
2022-01-27 00:11:17 +0530

    Added launcher program using executor service

--- producer/src/main/java/com/rohan/producer/Bootstrap.java
+++ producer/src/main/java/com/rohan/producer/Bootstrap.java
@@ -1,10 +1,33 @@
 package com.rohan.producer;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.logging.Logger;
+
 /*
  * Boostrap program to start the run the producer
  * */
 public class Bootstrap {
+	
+	private static final Logger LOG = Logger.getLogger(Bootstrap.class.getName());
+	
 	public static void main(String[] args) {
 		
+		LOG.info("Starting the producer thread");
+		
+		ExecutorService executorService = Executors.newFixedThreadPool(1);
+		try {
+			executorService = Executors.newFixedThreadPool(1);
+			Future<?> future = executorService.submit(new Producer());
+			
+			future.get();
+		} catch (Exception e) {
+			LOG.info("Exception occured " + e.getMessage());
+		} finally {
+			com.rohan.util.Utils.stop(executorService);
+		}
+		
+		LOG.info("Producer thread is over now");
 	}
 }
---
+++ producer/src/main/java/com/rohan/util/Utils.java
@@ -0,0 +1,22 @@
+package com.rohan.util;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Utils {
+	public static void stop(ExecutorService executor) {
+        try {
+            executor.shutdown();
+            executor.awaitTermination(60, TimeUnit.SECONDS);
+        }
+        catch (InterruptedException e) {
+            System.err.println("termination interrupted");
+        }
+        finally {
+            if (!executor.isTerminated()) {
+                System.err.println("killing non-finished tasks");
+            }
+            executor.shutdownNow();
+        }
+    }
+}
