56c7f7d06b7c4abbc692791df3bed11c754d5251
rohanseth497 <rohan.seth497@gmail.com>
2022-01-26 23:45:54 +0530

    Added kafka dependency and bootstrap classes

---
+++ producer/src/main/java/com/rohan/producer/Bootstrap.java
@@ -0,0 +1,10 @@
+package com.rohan.producer;
+
+/*
+ * Boostrap program to start the run the producer
+ * */
+public class Bootstrap {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ producer/src/main/java/com/rohan/producer/Producer.java
@@ -0,0 +1,11 @@
+package com.rohan.producer;
+
+/*
+ * Runnable to produce records for Kafka topic and push them regularly
+ * */
+public class Producer implements Runnable {
+
+	public void run() {
+		
+	}
+}
