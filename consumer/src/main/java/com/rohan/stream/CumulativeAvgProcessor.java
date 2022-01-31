b3fe581e8eff0afbde6741d6f0fbd9b2b40c2130
rohanseth497 <rohan.seth497@gmail.com>
2022-01-31 22:31:51 +0530

    Added Logic to compute and store cumulative average

--- consumer/src/main/java/com/rohan/stream/CumulativeAvgProcessor.java
+++ consumer/src/main/java/com/rohan/stream/CumulativeAvgProcessor.java
@@ -11,37 +11,44 @@ public class CumulativeAvgProcessor implements Processor<String, String> {
 	private static final Logger LOGGER = Logger.getLogger(CumulativeAvgProcessor.class.getName());
 	private static final String AVG_STORE_NAME = "in_memory_avg_store";
 	private static final String NUM_RECORDS_STORE_NAME = "in_memory_num_record_store";
-	
+
 	ProcessorContext pc = null;
 	private KeyValueStore<String, Double> machineToAvgCPUUsageStore;
 	private KeyValueStore<String, Integer> machineToNumberOfRecordsReadStore;
 
-	
 	@Override
 	public void init(ProcessorContext context) {
 		this.pc = context;
 		this.pc.schedule(12000); // punctuate after this interval
-		
+
 		this.machineToAvgCPUUsageStore = (KeyValueStore<String, Double>) pc.getStateStore(AVG_STORE_NAME);
-		this.machineToNumberOfRecordsReadStore = (KeyValueStore<String, Integer>) pc.getStateStore(NUM_RECORDS_STORE_NAME);
-		
+		this.machineToNumberOfRecordsReadStore = (KeyValueStore<String, Integer>) pc
+				.getStateStore(NUM_RECORDS_STORE_NAME);
+
 		LOGGER.info("Initializing the processor");
 	}
 
 	@Override
 	public void process(String machineId, String currentCPUUsage) {
 		LOGGER.info("Current machineId: " + machineId + " and usage is: " + currentCPUUsage);
-		
+
 		Double currentCPUUsageOfMachine = Double.parseDouble(currentCPUUsage);
 		Integer recordsReadForMachine = machineToNumberOfRecordsReadStore.get(machineId);
 		Double latestCumulativeAvg = null;
-		
+
 		if (recordsReadForMachine == null) {
 			machineToNumberOfRecordsReadStore.put(machineId, 1);
+			latestCumulativeAvg = currentCPUUsageOfMachine;
 		} else {
+
+			latestCumulativeAvg = (currentCPUUsageOfMachine + (recordsReadForMachine * currentCPUUsageOfMachine))
+					/ (recordsReadForMachine + 1);
+
+			LOGGER.info("Average for machine: " + machineId + " is: " + latestCumulativeAvg);
+
 			machineToNumberOfRecordsReadStore.put(machineId, recordsReadForMachine + 1);
 		}
-		
+
 		machineToAvgCPUUsageStore.put(machineId, latestCumulativeAvg);
 	}
 
